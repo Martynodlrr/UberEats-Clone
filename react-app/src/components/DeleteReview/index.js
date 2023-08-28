2b751b21a3a9fa09cee5e4549a804c1fb0817fc6
Amanda Morrow <a.holahan7@yahoo.com>
2023-08-28 08:28:15 -0700

    fixed errors in deleting a review

--- react-app/src/components/DeleteReview/index.js
+++ react-app/src/components/DeleteReview/index.js
@@ -1,23 +1,23 @@
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import * as spotReviews from '../../store/review';
+import * as reviewActions from '../../store/review';
 import './DeleteReview.css';
 
-export default function DeleteReview({reviewId}) {
+export default function DeleteReview({ reviewId }) {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
     // const reviewId = props.props;
 
     const reviewDelete = (e, reviewId) => {
         e.preventDefault();
-        dispatch(spotReviews.deleteReview(reviewId));
+        dispatch(reviewActions.deleteReview(reviewId));
         closeModal();
     };
     return (
         <>
             <div className='deleteReview'>
-                <h1 id='deleteSpotHead'>Confirm Delete</h1>
-                <h2 id='deleteSpotBlurb'>Are you sure you want to delete this review?</h2>
+                <h1 id='deleteReviewHead'>Confirm Delete</h1>
+                <h2 id='deleteReviewBlurb'>Are you sure you want to delete this review?</h2>
                 <button onClick={(e) => reviewDelete(e, reviewId)} id='deleteButton'>Yes (Delete Review)</button>
                 <button onClick={closeModal} id='dontDeleteButton'>No (Keep Review)</button>
             </div>
