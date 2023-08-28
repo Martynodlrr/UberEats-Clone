ebe1c21e5f01ac67c958df751be7c559ecf817c9
dudewithoneleg <galvancromeo@gmail.com>
2023-08-28 10:29:02 -0500

    review

--- react-app/src/components/CreateReview/index.js
+++ react-app/src/components/CreateReview/index.js
@@ -2,51 +2,51 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import * as reviewActions from '../../store/review';
-import './CreateReview.css';
+import './index.css';
 
-export default function CreateReview({ review, formType }) {
+export default function CreateReview({ currentReview, formType }) {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
-    const [review, setReview] = useState(formType === 'Update Review' ? review.review : '');
-    const [stars, setStars] = useState(formType === 'Update Review' ? review.stars : 0);
+    const [review, setReview] = useState(formType === 'Update Review' ? currentReview.review : '');
+    const [stars, setStars] = useState(formType === 'Update Review' ? currentReview.stars : 0);
     const [hover, setHover] = useState(null);
     const [errors, setErrors] = useState({});
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const newReview = {
-            review,
-            stars
-        };
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     const newReview = {
+    //         review,
+    //         stars
+    //     };
 
-        if (formType === 'Update Review') {
-            const returnFromThunk = reviewActions.updateReview(review, newReview);
-            const dbReview = await dispatch(returnFromThunk).catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) {
-                    setErrors(data.errors);
-                }
-            });
-            if (dbReview) {
-                history.push(`/reviews/${dbReview.id}`);
-            };
-        } else {
-            const returnFromThunk = reviewActions.createReview(newReview, spotId);
-            return dispatch(returnFromThunk).then(() => {
-                dispatch(reviewActions.allReviews(spotId));
-                closeModal();
-            }).catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) {
-                    setErrors(data.errors)
-                }
-            });
-        };
-    }
+    //     if (formType === 'Update Review') {
+    //         const returnFromThunk = reviewActions.updateReview(review, newReview);
+    //         const dbReview = await dispatch(returnFromThunk).catch(async (res) => {
+    //             const data = await res.json();
+    //             if (data && data.errors) {
+    //                 setErrors(data.errors);
+    //             }
+    //         });
+    //         if (dbReview) {
+    //             history.push(`/reviews/${dbReview.id}`);
+    //         };
+    //     } else {
+    //         const returnFromThunk = reviewActions.createReview(newReview, spotId);
+    //         return dispatch(returnFromThunk).then(() => {
+    //             dispatch(reviewActions.allReviews(spotId));
+    //             closeModal();
+    //         }).catch(async (res) => {
+    //             const data = await res.json();
+    //             if (data && data.errors) {
+    //                 setErrors(data.errors)
+    //             }
+    //         });
+    //     };
+    // }
 
     return (
         <>
-            <div className='reviewModal'>
+            {/* <div className='reviewModal'>
                 {formType === 'Update Spot' ? <h1>Update your Review</h1> : <h1>Create a New Review</h1>}
                 <form onSubmit={handleSubmit}>
                     {errors.message && <p>{errors.message}</p>}
@@ -78,7 +78,7 @@ export default function CreateReview({ review, formType }) {
                     </div>
                     <button type='submit' className='signupSubmit' disabled={review.length < 10 || stars === 0}>Submit Your Review</button>
                 </form>
-            </div>
+            </div> */}
         </>
     )
 }
