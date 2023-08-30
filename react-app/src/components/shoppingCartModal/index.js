edeb218ce8f65ceef8bcc7d2e06130581590fb0c
hunter12756 <hlwatson17256@gmail.com>
2023-08-30 18:59:35 -0400

    shopping cart CSS

--- react-app/src/components/shoppingCartModal/index.js
+++ react-app/src/components/shoppingCartModal/index.js
@@ -47,12 +47,14 @@ function ShoppingCartModal({ state, item, restaurant }) {
               total += item.price;
               return (
                 <li key={item.id} className='cart-list'>
-                  <h4>{item.name}</h4>
-                  <p>${item.price}</p>
+                  <h4 >{item.name}</h4>
+                  <p >${item.price}</p>
                   <TiDelete onClick={() => handleSingleItemDelete(item.id)} />
                 </li>
               );
             })}
+            <div className='checkout-container'>
+
           {cart && Object.values(cart).length > 1 && (
             <button
               id='checkout-button'
@@ -60,6 +62,7 @@ function ShoppingCartModal({ state, item, restaurant }) {
             >Checkout</button>
           )}
           <h4 id='total'>Total: ${total.toFixed(2)}</h4>
+            </div>
         </div>
       }
     </>
